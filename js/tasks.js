c7e56a92e7795130c079b76848e570bdd08129e6
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2021-04-08 15:57:35 +0000

    add node_modules to gitignore

--- js/tasks.js
+++ js/tasks.js
@@ -6,14 +6,18 @@ const taskslistContainer = document.getElementById("tasks-list-container");
 const pageContainer = document.getElementById("page-container")
 const tasks = [];
 
+// gets input value and saves it task to an array 
 function createTask(e) {
     e.preventDefault();
     const taskValue = addTaskInput.value;
 
-    console.log(taskValue);
+    //store input value on an array and then in localStorage
+    tasks.push(taskValue);
+    setLocalStorage(tasks);
 
     // creates a new input field while user doesn't submit
     renderTask(taskValue);
+    
     //clear input field
     addTaskInput.value="";
 }
@@ -25,21 +29,22 @@ function renderTask(taskValue) {
     
     // sets input value to paragraph
     p.innerHTML = `<p class="task">${taskValue}</p>`
+    appendRemoveButton(p);
 
     // create task-container and add
     const taskContainer = document.createElement("div");
     taskContainer.classList.add("task-container");
 
-    // append paragraph to 
+    // append paragraph to task-container
     taskContainer.appendChild(p);
 
     // append task container to task list container
     taskslistContainer.appendChild(taskContainer);
-    
-    //store input value somewhere else
-    tasks.push(taskValue);
+}
 
-    setLocalStorage(tasks);
+// user can remove task before submitting tasks list
+function removeTask() {
+
 }
 
 // make sure application has persistence on browser
@@ -53,6 +58,18 @@ function getLocalStorage() {
     const store = JSON.parse(localStorage.getItem("tasks"));
     // turn object into array to use foreach function and display tasks
     Array.from(store).forEach(elem => renderTask(elem));
+}
+
+// append a remove button on each task before user decide to submit
+function appendRemoveButton(element) {
+    
+    const button = document.createElement("button");
+    button.classList.add("remove-button");
+    const buttonText = document.createTextNode("cancel");
+    button.appendChild(buttonText);
+    button.addEventListener("click", removeTask);
+
+    element.appendChild(button);
 }
 
 // event listeners
